8fe520c9ea5d754192f8c9cd4ddf9b489e781d94
Brandon <brand.ld@1994@gmail.com>
2017-02-11 09:57:56 +0000

    Created Game Manager and Loader Class
    Able to spawn enemies now

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+public static GameManager instance = null;
+public GameObject spawnPoint;
+public GameObject[] enemies;
+public int maxOnScreenEnemies;
+public int totalEnemies;
+public int enemiesPerSpawn;
+
+private int enemiesOnScreen = 0;
 	
-	// Update is called once per frame
-	void Update () {
+
+/// <summary>
+/// Awake is called when the script instance is being loaded.
+/// </summary>
+	void Awake()
+	{
+		if(instance == null){
+			instance = this;
+		}
+		else if(instance != this){
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
 		
 	}
+// Use this for initialization
+	void Start () {
+		SpawnEnemy();
+	}
+
+	void SpawnEnemy(){
+		if(enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies){
+			for(int i = 0; i < enemiesPerSpawn; i++){
+				if(enemiesOnScreen < maxOnScreenEnemies){
+					GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+					// instatiate creates an object, using as game object turns the object back into a game object
+					newEnemy.transform.position = spawnPoint.transform.position;
+					maxOnScreenEnemies += 1;
+				}
+			}
+		}
+	}	
 }
+
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject gameManager;
+
+	 /// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		if(GameManager.instance == null){
+			Instantiate(gameManager);
+		}
+
+	}
+		
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
