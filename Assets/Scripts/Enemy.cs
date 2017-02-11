394cbbb23da540f1065ac46ea760bad3bc45327e
Brandon <brand.ld@1994@gmail.com>
2017-02-11 10:20:06 +0000

    Starting Enemy Pathfinding

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+public int target = 0;
+public Transform exitPoint;
+
+public Transform[] wayPoints;
+
+//the smaller the number the less it will be called in update
+public float navigationUpdate;
+
+
+private Transform enemy;
+private float navigationTime = 0;
+
+
 	// Use this for initialization
 	void Start () {
+		enemy = GetComponent<Transform>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(wayPoints != null){
+			navigationTime += Time.deltaTime;
+			if(navigationTime > navigationUpdate){
+				if(target < wayPoints.Length){
+					enemy.position = Vector2.MoveTowards(enemy.position,wayPoints[target].position, navigationTime);
+				}else {
+					enemy.position = Vector2.MoveTowards(enemy.position,exitPoint.position, navigationTime);
+				}
+				navigationTime = 0;
+			}
+		}
 	}
+
 }
