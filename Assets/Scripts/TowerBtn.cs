efc00dc0b99b3af5b56f9e64201b57ea619c34f0
Brandon <brand.ld@1994@gmail.com>
2017-02-14 13:30:32 +0000

    Created Tower Manager class
    Also got buttons implemented they are currently still in testing stages
    they debug messages rather than instantiate objects that will be the
    next step.

---
+++ Assets/Scripts/TowerBtn.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class TowerBtn : MonoBehaviour {
+
+[SerializeField]
+private GameObject towerObject;
+
+public GameObject TowerObject{
+		get{
+			return towerObject;
+			}
+		}
+}
