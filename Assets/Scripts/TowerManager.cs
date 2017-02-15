30a76ee0cfccee65b8f03da5e32d3e0fd714bbcf
Brandon <brand.ld@1994@gmail.com>
2017-02-15 15:53:41 +0000

    Can now place towers on build sites
    implemented 2D raycasts to find the point we clicked and if we click on
    the button then an area which is tagged as a build site we can
    instantiate a tower.
    Also changed sorting layer settings on the towers

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Singleton<TowerManager> {
 
@@ -13,12 +14,28 @@ public class TowerManager : Singleton<TowerManager> {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0)){
+			// Transforms position from screen space into world space.
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			// raycasts, pretty much shoot a line into the scene until it hits something 
+			RaycastHit2D hit = Physics2D.Raycast(worldPoint,Vector2.zero);
+			if(hit.collider.tag == "BuildSite"){
+				PlaceTower(hit);
+			}
+
+
+		}
+	}
+	public void PlaceTower(RaycastHit2D hit){
+		//if pointer over game object is false and the tower button has been pressed this will work
+		if(!EventSystem.current.IsPointerOverGameObject() && towerBtnPressed != null){
+			GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
+			newTower.transform.position = hit.transform.position;
+		}
 	}
 
 
 	public void SelectedTower(TowerBtn towerSelected){
 		towerBtnPressed = towerSelected;
-		Debug.Log("Pressed!: " + towerBtnPressed.gameObject);
 	}
 }
