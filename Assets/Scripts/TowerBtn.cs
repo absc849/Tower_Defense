9f0475f3b08dd6a5a8633e03831630094178ade7
Brandon <brand.ld@1994@gmail.com>
2017-02-17 13:30:57 +0000

    Enabled sprite drag
    added a drag sprite variable to the buttons and implemented a few
    methods in tower manager which enable the sprite renderer and have the
    sprite follow the mouse before you place the sprite on the build site.
    also fixed build site error where if you click the site and the button,
    the tag changes and you can no longer place build sites

--- Assets/Scripts/TowerBtn.cs
+++ Assets/Scripts/TowerBtn.cs
@@ -5,10 +5,20 @@ public class TowerBtn : MonoBehaviour {
 
 [SerializeField]
 private GameObject towerObject;
+[SerializeField]
+private Sprite dragSprite;
 
 public GameObject TowerObject{
 		get{
 			return towerObject;
 			}
 		}
+
+
+public Sprite DragSprite{
+		get{
+			return dragSprite;
+			}
+		}
 }
+
--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -6,10 +6,13 @@ using UnityEngine.EventSystems;
 public class TowerManager : Singleton<TowerManager> {
 
 	private TowerBtn towerBtnPressed;
+	private SpriteRenderer spriteRenderer;
+
+
 
 	// Use this for initialization
 	void Start () {
-		
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -19,23 +22,44 @@ public class TowerManager : Singleton<TowerManager> {
 			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			// raycasts, pretty much shoot a line into the scene until it hits something 
 			RaycastHit2D hit = Physics2D.Raycast(worldPoint,Vector2.zero);
-			if(hit.collider.tag == "BuildSite"){
+			if(hit.collider.tag == "BuildSite" && towerBtnPressed != null){
 				PlaceTower(hit);
 			}
-
-
 		}
+
+		if(spriteRenderer.enabled){
+				FollowMouse();
+				transform.position = new Vector2(transform.position.x, transform.position.y);
+			}
 	}
 	public void PlaceTower(RaycastHit2D hit){
 		//if pointer over game object is false and the tower button has been pressed this will work
 		if(!EventSystem.current.IsPointerOverGameObject() && towerBtnPressed != null){
+			hit.collider.tag = "BuildSiteFull";
 			GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
 			newTower.transform.position = hit.transform.position;
+			DisableDragSprite();
 		}
 	}
 
 
 	public void SelectedTower(TowerBtn towerSelected){
 		towerBtnPressed = towerSelected;
+		EnableDragSprite(towerBtnPressed.DragSprite);
+	}
+
+	public void FollowMouse(){
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	public void EnableDragSprite(Sprite sprite){
+		spriteRenderer.enabled = true;
+		spriteRenderer.sprite = sprite;
+
+	}
+
+	public void DisableDragSprite(){
+		spriteRenderer.enabled = false;
+
 	}
 }
